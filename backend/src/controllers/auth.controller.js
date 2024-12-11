94aeffc9a507b9904f3e918bd28bb5e0c8255e55
manjeet <manjeetkarnwal1027@gmail.com>
2024-12-11 23:02:46 +0530

    login and logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -49,10 +49,44 @@ export const signup = async(req,res) => {
   }
 };
 
-export const login = (req,res) => {
-  res.send("login route");
+export const login = async (req,res) => {
+  const {email, password} = req.body;
+
+  try{
+    const user = await User.findOne({email});
+
+    if(!user){
+      return res.status(400).json({message:"Invalid credentials"});
+    }
+
+    const isPassCorrect = await bcrypt.compare(password, user.password);
+
+    if(!isPassCorrect){
+      return res.status(400).json({message:"Invalid Credentials"});
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic
+    });
+  }
+  catch(err){
+    console.log("Error in login", err.message);
+    res.status(500).json({Message:"Internal Server Error"});
+  }
 };
 
 export const logout = (req,res) => {
-  res.send("logout route");
+  try{
+    res.cookie("jwt","",{maxAge: 0});
+    res.status(200).json({message:"Logged out succcessfully"});
+  }
+  catch(err){
+    console.log("Error in logout", err.message);
+    res.status(500).json({Message:"Internal Server Error"});
+  }
 };
