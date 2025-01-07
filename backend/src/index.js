b2329fa7b053948e090cf343991811a243e2f20f
manjeet <manjeetkarnwal1027@gmail.com>
2025-01-08 01:10:45 +0530

    Signup & login page, logout

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import express from "express";    // package -> type : module
 import cookieParser from "cookie-parser"
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors"
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
@@ -14,7 +15,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json());  // help in extracting the json data out of body in auth.controller.js
 app.use(cookieParser());  // help to parse the cookie
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
